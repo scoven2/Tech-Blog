f599dda06505c7c88b501367ca2f9f642d25321f
Scott Siegel <scottmatthewsiegel@gmail.com>
2021-07-23 01:49:58 -0500

    put back sequelize const in comment-routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -13,7 +13,7 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-    Comment.findAll({
+    Comment.findOne({
             where: {
                 id: req.params.id,
             },
