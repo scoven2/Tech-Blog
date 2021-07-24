baf156f2729520a793622820ddf202b83a2f1af9
Scott Siegel <scottmatthewsiegel@gmail.com>
2021-07-24 18:50:07 -0500

    created comment, delete, edit, login, logout, and signup form handlers in public js file

--- public/js/add-post.js
+++ public/js/add-post.js
@@ -2,7 +2,9 @@ async function newFormHandler(event) {
     event.preventDefault();
 
     const title = document.querySelector('input[name="post-title"]').value;
+
     const content = document.querySelector('textarea[name="content"]').value;
+
     const response = await fetch(`/api/post`, {
         method: 'POST',
         body: JSON.stringify({
